543b7136552e4c0092a94b471a5c404ea6cc1a96
krzx <104647540+krzx000@users.noreply.github.com>
2024-03-03 23:42:35 +0100

    Fixed after-join problem. (#1)

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import http from 'http';
+import express from "express";
+import http from "http";
 // import places from './src/data/places.js';
-import { Server as socketIoModule } from 'socket.io';
+import { Server as socketIoModule } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new socketIoModule(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
-
 });
 
 let places = [];
@@ -19,20 +18,33 @@ server.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
 });
 
-io.on('connection', socket => {
-  console.log('Nowy klient połączony');
-  io.emit('placesData', places);
+io.on("connection", (socket) => {
+  console.log("Nowy klient połączony");
 
-  socket.on('newPlace', newPlace => {
-    console.log('Nowe miejsce:', newPlace);
-    places.push({id: socket.id, place : newPlace});
+  socket.on("newPlace", (newPlace) => {
+    console.log("Nowe miejsce:", newPlace);
+    places.push({ id: socket.id, place: newPlace });
     //check if newPlace just exist
-    io.emit('placesData', places.map(place => place.place));
+    io.emit(
+      "placesData",
+      places.map((place) => place.place)
+    );
   });
 
-  socket.on('disconnect', () => {
-    console.log('Klient rozłączony');
-    places = places.filter(place => place.id !== socket.id);  
-    io.emit('placesData', places.map(place => place.place));  
+  socket.on("getPlaces", () => {
+    console.log("Zażądano miejsc");
+    io.emit(
+      "placesData",
+      places.map((place) => place.place)
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Klient rozłączony");
+    places = places.filter((place) => place.id !== socket.id);
+    io.emit(
+      "placesData",
+      places.map((place) => place.place)
+    );
   });
 });
