a1741766334fbe87bc9fd985a382416fdeb6271a
Rafał Taraszka <rtaraszka@poczta.fm>
2024-04-01 21:32:34 +0200

    Refactoring to GlobeComponent

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import arcsData from "./data/data.js";
 import {useRef, useState, useEffect} from "react";
 import {io} from "socket.io-client";
 import  ErrorMessage from "./components/ErrorMessage.js";
+import GlobeComponent from "./components/GlobeComponent.js";
 
 const AUTO_ROTATE_SPEED = 0.5;
 const POV_POSITION_TIME = 20000;
@@ -110,37 +111,7 @@ function App() {
                     Go to my location
                 </button>
             </div>
-            <Globe
-                pointOfView
-                ref={globeEl}
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-                // edges
-                arcsData={arcsData}
-                arcColor={"color"}
-                //arcDashLength={() => 0.5}
-                arcDashGap={(d) => 1 - (d.stroke - 0.1)}
-                arcDashAnimateTime={(d) => 5000}
-                arcStroke={"stroke"}
-                //arcCircularResolution={64}
-                // arcLabel={() => "test"}
-                // labels
-                labelsData={places}
-                labelLat={(d) => d.lat}
-                labelLng={(d) => d.lng}
-                labelText={(d) => d.name}
-                labelSize={(d) => 0.5 + d.size}
-                labelDotRadius={(d) => 0.5 + d.size}
-                labelColor={() => "rgba(255, 165, 0, 0.75)"}
-                labelResolution={2}
-                // bars
-                hexBinPointsData={places}
-                hexBinPointWeight="size"
-                hexAltitude={(d) => d.sumWeight - 0.1 + 0.05}
-                hexBinResolution={4}
-                hexBinMerge={true}
-                enablePointerInteraction={false}
-            />
+            <GlobeComponent places={places} globeEl={globeEl} />
         </div>
     );
 }
