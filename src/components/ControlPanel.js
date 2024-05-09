a4cea3dfca4d4af1bb90939a0994caf30c20f72f
Rafał Taraszka <rtaraszka@poczta.fm>
2024-05-09 11:00:42 +0200

    Add variables theme, as Sass,

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -3,10 +3,33 @@ import "./ControlPanel.scss";
 
 const POV_POSITION_TIME = 20000;
 
-function ControlPanel({handleMyLocation, handleJoin, globeElement, setError}) {
+function ControlPanel({ handleMyLocation, handleJoin, globeElement, setError }) {
     return (
         <div className="control-panel">
             <h1>Control Panel</h1>
+            <div class="accordion">
+
+                <div class="accordion-block">
+                    <button class="accordion-btn">Sekcja 1</button>
+                    <div class="accordion-content hidden">
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="accordion-block">
+                    <button class="accordion-btn">Sekcja 2</button>
+                    <div class="accordion-content hidden">
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="accordion-block">
+                    <button class="accordion-btn">Sekcja 3</button>
+                    <div class="accordion-content hidden">
+                        <p>Treść sekcji 3...</p>
+                    </div>
+                </div>
+            </div>
             <div className="control-panel__buttons">
                 <button onClick={handleMyLocation}>My Location</button>
                 <button onClick={handleJoin}>Join to</button>
