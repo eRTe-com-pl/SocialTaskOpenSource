23fa118a00de91dcb10630d6c9fd790800d69793
Rafał Taraszka <rafal@hekacode.com>
2024-12-04 00:09:46 +0100

    Add bootstrap elements in Panel

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -3,39 +3,73 @@ import "./ControlPanel.scss";
 
 const POV_POSITION_TIME = 20000;
 
-function ControlPanel({ handleMyLocation, handleJoin, globeElement, setError }) {
+function ControlPanel({handleMyLocation, handleJoin, globeElement, setError}) {
     return (
         <div className="control-panel">
             <h1>Control Panel</h1>
-            <div class="accordion">
 
-                <div class="accordion-block">
-                    <button class="accordion-btn">Sekcja 1</button>
-                    <div class="accordion-content hidden">
-                        <p></p>
-                    </div>
-                </div>
-
-                <div class="accordion-block">
-                    <button class="accordion-btn">Sekcja 2</button>
-                    <div class="accordion-content hidden">
-                        <p></p>
-                    </div>
-                </div>
-
-                <div class="accordion-block">
-                    <button class="accordion-btn">Sekcja 3</button>
-                    <div class="accordion-content hidden">
-                        <p>Treść sekcji 3...</p>
-                    </div>
-                </div>
-            </div>
             <div className="control-panel__buttons">
                 <button onClick={handleMyLocation}>My Location</button>
                 <button onClick={handleJoin}>Join to</button>
-                <a href="https://getbootstrap.com/docs/5.3/examples/offcanvas-navbar/" target="_blank" rel="noopener noreferrer">https://getbootstrap.com/docs/5.3/examples/offcanvas-navbar/</a>
             </div>
 
+            <div className="my-3 p-3 bg-body rounded shadow-sm">
+                <h6 className="border-bottom pb-2 mb-0">Suggestions</h6>
+                <div className="d-flex text-body-secondary pt-3">
+                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#007bff"></rect>
+                        <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
+                    </svg>
+                    <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                        <div className="d-flex justify-content-between">
+                            <strong className="text-gray-dark">Full Name</strong>
+                            <button type="button" className="btn btn-primary btn-sm position-relative rounded-4">
+                                Join
+                                <span
+                                    className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span className="visually-hidden">unread messages</span>
+  </span>
+                            </button>
+                        </div>
+                        <span className="d-block left">@username</span>
+                    </div>
+                </div>
+                <div className="d-flex text-body-secondary pt-3">
+                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#007bff"></rect>
+                        <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
+                    </svg>
+                    <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                        <div className="d-flex justify-content-between">
+                            <strong className="text-gray-dark">Full Name</strong>
+                            <a href="#">Follow</a>
+                        </div>
+                        <span className="d-block left">@username</span>
+                    </div>
+                </div>
+                <div className="d-flex text-body-secondary pt-3">
+                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                         xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                         preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#007bff"></rect>
+                        <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
+                    </svg>
+                    <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
+                        <div className="d-flex justify-content-between">
+                            <strong className="text-gray-dark">Full Name</strong>
+                            <a href="#">Follow</a>
+                        </div>
+                        <span className="d-block left">@username</span>
+                    </div>
+                </div>
+                <div className="d-flex text-body-secondary pt-3">
+                </div>
+            </div>
         </div>
     );
 }
